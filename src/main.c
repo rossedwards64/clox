ddd809e36ebb540226485e4430232fbc5470f58f
Ross Edwards <redwards64@hotmail.com>
2023-05-11 08:45:25 +0100

    implement stack and arithmetic

--- src/main.c
+++ src/main.c
@@ -1,15 +1,28 @@
+#include "vm.h"
 #include "chunk.h"
 #include "debug.h"
 
 int main(int argc, const char **argv)
 {
+    init_vm();
     chunk_t chunk;
     init_chunk(&chunk);
     int constant = add_constant(&chunk, 1.2);
     write_chunk(&chunk, OP_CONSTANT, 123);
     write_chunk(&chunk, constant, 123);
+    constant = add_constant(&chunk, 3.4);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
+    write_chunk(&chunk, OP_ADD, 123);
+    constant = add_constant(&chunk, 5.6);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
+    write_chunk(&chunk, OP_DIVIDE, 123);
+    write_chunk(&chunk, OP_NEGATE, 123);
     write_chunk(&chunk, OP_RETURN, 123);
-    disassemble_chunk(&chunk, "test chunk");
+    //disassemble_chunk(&chunk, "test chunk");
+    interpret(&chunk);
+    free_vm();
     free_chunk(&chunk);
     return 0;
 }
---
+++ src/vm.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+#include "vm.h"
+#include "debug.h"
+#include "chunk.h"
+#include "value.h"
+
+vm_t vm;
+
+static void reset_stack()
+{
+    vm.stack_top = vm.stack;
+}
+
+void init_vm()
+{
+    reset_stack();
+}
+
+void free_vm()
+{
+
+}
+
+static interpret_result_t run()
+{
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
+
+    for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("        ");
+        for (value_t *slot = vm.stack; slot < vm.stack_top; slot++) {
+            printf("[ ");
+            print_value(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassemble_instruction(vm.chunk,
+                                (int)(vm.ip - vm.chunk->code));
+#endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                value_t constant = READ_CONSTANT();
+                push(constant);
+                break;
+            }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
+            case OP_RETURN: return INTERPRET_OK;
+        }
+    }
+#undef READ_CONSTANT
+#undef BINARY_OP
+#undef READ_BYTE
+}
+
+interpret_result_t interpret(chunk_t *chunk)
+{
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
+
+void push(value_t value)
+{
+    *vm.stack_top = value;
+    vm.stack_top++;
+}
+
+value_t pop()
+{
+    vm.stack_top--;
+    return *vm.stack_top;
+}
